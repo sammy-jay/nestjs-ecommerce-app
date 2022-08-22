6827c72a4c22b0ba7df6453670cde6a853e2055d
Tolu Soneye <sammyjayden27@gmail.com>
2022-08-22 02:07:56 +0000

    added business logic to the service file and created all needed routes

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Query,
+  Body,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDTO } from './dtos/create-product.dto';
+import { FilterProductDTO } from './dtos/filter-product.dto';
 
-@Controller('product')
-export class ProductController {}
+@Controller('store/product')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get('/')
+  async getProducts(@Query() filterProductDTO: FilterProductDTO) {
+    if (Object.keys(filterProductDTO).length) {
+      const filteredProducts = await this.productService.getFilteredProducts(
+        filterProductDTO,
+      );
+      return filteredProducts;
+    } else {
+      const allProducts = await this.productService.getAllProducts();
+      return allProducts;
+    }
+  }
+
+  @Get('/:id')
+  async getProduct(@Param('id') id: string) {
+    const product = await this.productService.getProduct(id);
+    if (!product) throw new NotFoundException(`Product ${id} not found`);
+    return product;
+  }
+
+  @Post('/')
+  async addProduct(@Body() createProductDTO: CreateProductDTO) {
+    const product = await this.productService.addProduct(createProductDTO);
+    return product;
+  }
+
+  @Put('/:id')
+  async updateProduct(
+    @Param('id') id: string,
+    @Body() updateProductDTO: CreateProductDTO,
+  ) {
+    const product = await this.productService.updateProduct(
+      id,
+      updateProductDTO,
+    );
+    return product;
+  }
+
+  @Delete('/:id')
+  async deleteProduct(@Param('id') id: string) {
+    const product = await this.productService.deleteProduct(id);
+    if (!product) throw new NotFoundException(`Product ${id} not found`);
+    return product;
+  }
+}
